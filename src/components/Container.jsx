9571252a0e2edcbd2a7a455bf4ee6b707f50f525
ssudharshan-s <s.shanfsd@gmail.com>
2025-09-04 17:42:07 +0530

    Updated files and added resume PDF

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,6 +3,7 @@ import './Container.css';
 import img from '../Assets/header-img.svg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import resume from "../Assets/1.pdf";
 const Container = () =>{
     useEffect(() => {
         AOS.init({duration: 1000}); // AOS initialization with a duration of 1000ms
@@ -13,7 +14,10 @@ const Container = () =>{
                 <h2 >Hi! I'm Sudharshan
                      Web Developer</h2>
                 <p>This is My portfolio</p>
-                <a className="con" href="/">Let's Connect</a>
+<a className="con" href={resume} target="_blank" rel="noopener noreferrer">
+  Resume
+</a>
+               
             </div>
             <div className="image">
                <img className="img" src={img} alt=""></img>
